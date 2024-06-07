4554e78087529d8b08999377202de7502f405ce5
Sylvain Ard <sylvain.ard@gmail.com>
2024-06-07 18:12:52 +0200

    correction main.tf et ajouts fichiers de rapport

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  name                 = "mydb"
+  identifier           = "mydb-instance"
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
