2887d9f78d20af1492c0f4b8a9a41a9abac666b6
Sylvain Ard <sylvain.ard@gmail.com>
2024-06-08 01:06:30 +0200

    correction de main.tf et deploy.yaml

--- main.tf
+++ main.tf
@@ -99,36 +99,9 @@ resource "aws_instance" "frontend_instance" {
               sudo yum install nginx -y
               sudo systemctl enable nginx
               sudo systemctl start nginx
-              sudo tee /etc/nginx/nginx.conf > /dev/null <<EOL
-              user nginx;
-              worker_processes auto;
-              error_log /var/log/nginx/error.log;
-              pid /run/nginx.pid;
 
-              events {
-                  worker_connections 1024;
-              }
-
-              http {
-                  log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                                    '$status $body_bytes_sent "$http_referer" '
-                                    '"$http_user_agent" "$http_x_forwarded_for"';
-
-                  access_log  /var/log/nginx/access.log  main;
-
-                  sendfile            on;
-                  tcp_nopush          on;
-                  tcp_nodelay         on;
-                  keepalive_timeout   65;
-                  types_hash_max_size 2048;
-
-                  include             /etc/nginx/mime.types;
-                  default_type        application/octet-stream;
-
-                  include /etc/nginx/conf.d/*.conf;
-              }
-              EOL
-              sudo tee /etc/nginx/conf.d/default.conf > /dev/null <<EOL
+              # Configure Nginx to serve the React application
+              sudo cat > /etc/nginx/conf.d/default.conf <<EOL
               server {
                   listen 80;
                   server_name _;
@@ -137,10 +110,11 @@ resource "aws_instance" "frontend_instance" {
                   index index.html;
 
                   location / {
-                      try_files \$uri /index.html;
+                      try_files \$uri \$uri/ /index.html;
                   }
               }
               EOL
+
               sudo systemctl restart nginx
               EOF
 
