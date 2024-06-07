74272dfe89a78be46d705e1a4b0be54be3d4a84b
Sylvain Ard <sylvain.ard@gmail.com>
2024-06-07 21:01:46 +0200

    batch dev

--- push dev vers main.bat
+++ push dev vers main.bat
@@ -1,14 +1,5 @@
-# Mettre à jour les branches locales
 git fetch origin
-
-# Basculer vers la branche main
 git checkout main
-
-# Mettre à jour la branche main
 git pull origin main
-
-# Fusionner la branche dev dans la branche main
 git merge dev
-
-# Pousser les modifications sur le dépôt distant
 git push origin main
